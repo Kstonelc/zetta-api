cdad06d1cb55b1002719e264a347f10eba738fa3
liuchang <liuchang@yrobot.com>
2025-07-22 08:46:20 +0800

    update

--- models/user.py
+++ models/user.py
@@ -5,5 +5,7 @@ from .base import BaseModel
 class User(BaseModel):
     __tablename__ = "user"
 
+    avatar = Column(String, nullable=False)
     email = Column(String, unique=True, index=True, nullable=False)
     password = Column(String, nullable=False)
+    secret = Column(String, nullable=True)  # 用于邮箱验证的密钥
